a04aa85bbb4a73fe15e047a55b8dadea676aaad6
Vivekanand P V <vivekanandpv@outlook.com>
2024-05-18 12:08:21 +0530

    done

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,46 @@
 import java.io.*;
-import java.nio.file.Path;
+import java.nio.file.*;
 
 public class Main {
     public static void main(String[] args) {
+        //  Files helper class is added in Java 7 as part of NIO.2
+        //  This class provides many useful functions in a straight-forward way
+        //  https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/nio/file/Files.html
         copyFileBinary();
+        copyFileCharacter();
+        readAndStream();
     }
 
     static void copyFileBinary() {
-        Path originalFilePath = Path.of("earth.png");
-        Path copyFilePath = Path.of("earth-copy.png");
-
-        try (
-                InputStream inputStream = new BufferedInputStream(new FileInputStream(originalFilePath.toFile()));
-                OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(copyFilePath.toFile()))
-        ) {
-
-            outputStream.write(inputStream.readAllBytes());
-
-            System.out.println("\nEnd of file copying");
-
-        } catch (FileNotFoundException e) {
+        try {
+            Files.write(
+                    Path.of("earth-copy.png"),
+                    Files.readAllBytes(Path.of("earth.png"))
+            );
+        } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    static void copyFileCharacter() {
+        try {
+            Files.write(
+                    Path.of("sample-copy.txt"),
+                    Files.readAllLines(Path.of("sample.txt"))
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void readAndStream() {
+        try {
+            //  Since readAllLines returns List<String>,
+            //  it can be combined with stream processing capabilities
+            Files.readAllLines(Path.of("sample.txt"))
+                    .stream()
+                    .map(String::toUpperCase)
+                    .forEach(System.out::println);
         } catch (IOException e) {
             e.printStackTrace();
         }
