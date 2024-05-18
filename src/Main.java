36bc1013f0accdcc045b9b9b00776d71a22bd707
Vivekanand P V <vivekanandpv@outlook.com>
2024-05-18 17:27:22 +0530

    done

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,34 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         //  In Windows, install telnet client through Windows Features
         //  In PowerShell: telnet 127.0.0.1 3000
         //  This application mirrors your input
-        try (ServerSocket serverSocket = new ServerSocket(3000);
-             Socket incomingRequest = serverSocket.accept();
+        //  Create multiple telnet sessions.
+        //  Every session will get a different thread from the pool.
+
+        try (ExecutorService service = Executors.newFixedThreadPool(4);
+             ServerSocket serverSocket = new ServerSocket(3000)
+        ) {
+            while (true) {
+                service.submit(() -> {
+                    try {
+                        handleRequest(serverSocket);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        }
+    }
+
+    static void handleRequest(ServerSocket serverSocket) throws IOException {
+        try (Socket incomingRequest = serverSocket.accept();
              InputStream inputStream = incomingRequest.getInputStream();
              OutputStream outputStream = incomingRequest.getOutputStream();
              Scanner scanner = new Scanner(inputStream, "UTF-8");
@@ -24,8 +44,8 @@ public class Main {
                     break;
                 }
 
-                System.out.printf("To Terminal >> Received: %s\n", line);
-                printWriter.printf(">> Server mirrors: %s\n", line);
+                System.out.printf("To Terminal [%s] >> Received: %s\n", Thread.currentThread().getName(), line);
+                printWriter.printf(">> [%s] Server mirrors: %s\n", Thread.currentThread().getName(), line);
             }
         }
     }
